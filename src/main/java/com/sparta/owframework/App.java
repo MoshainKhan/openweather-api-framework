f323eeba6b642285311f8bea00143615169ae930
Josh A <joshaustin312@gmail.com>
2022-07-11 14:45:33 +0100

    Initial commit

---
+++ src/main/java/com/sparta/owframework/App.java
@@ -0,0 +1,13 @@
+package com.sparta.owframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
