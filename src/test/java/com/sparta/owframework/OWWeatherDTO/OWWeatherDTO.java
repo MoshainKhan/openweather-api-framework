3ec5435d21c896b1033db8fc201c32f7e8fc300b
J Hibbert <jhibbert@spartaglobal.com>
2022-07-12 15:28:02 +0100

    added check for current date against date a request was set

--- src/test/java/com/sparta/owframework/OWWeatherDTO/OWWeatherDTO.java
+++ src/test/java/com/sparta/owframework/OWWeatherDTO/OWWeatherDTO.java
@@ -1,5 +1,7 @@
 package com.sparta.owframework.OWWeatherDTO;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -118,4 +120,12 @@ public class OWWeatherDTO {
 			return true;
 		} else return timezone % 3600 == 0;
 	}
+
+	public boolean requestDateSentCorrect() {
+		Date time = new Date((dt) * 1000L);
+		String requestDate = time.toString();
+		String currentDate = LocalDate.now().toString();
+		return requestDate.contains(currentDate);
+	}
+
 }
