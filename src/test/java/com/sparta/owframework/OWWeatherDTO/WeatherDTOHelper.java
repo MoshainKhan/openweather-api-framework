7c93816f3fe71c041fccc84b4fcec9c048aca7c5
Emem Etteh <eetteh@spartaglobal.com>
2022-07-12 16:21:03 +0100

    removed parameters and used && in place of ||

--- src/test/java/com/sparta/owframework/OWWeatherDTO/WeatherDTOHelper.java
+++ src/test/java/com/sparta/owframework/OWWeatherDTO/WeatherDTOHelper.java
@@ -3,7 +3,7 @@ package com.sparta.owframework.OWWeatherDTO;
 public class WeatherDTOHelper {
 
     public boolean isBetween0And100(int number) {
-        return number >= 0 || number <= 100;
+        return number >= 0 && number <= 100;
     }
 
     public boolean isNotLessThan0(double value) {
