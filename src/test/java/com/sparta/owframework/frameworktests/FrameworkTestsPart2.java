5a85b7aa59af329501248c7890c419a80f98a1ea
Adelaide Baron <16630927@students.lincoln.ac.uk>
2022-07-14 12:25:57 +0100

    correcting tests

--- src/test/java/com/sparta/owframework/frameworktests/FrameworkTestsPart2.java
+++ src/test/java/com/sparta/owframework/frameworktests/FrameworkTestsPart2.java
@@ -1,4 +1,3 @@
-/*
 package com.sparta.owframework.frameworktests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,142 +6,57 @@ import org.junit.jupiter.api.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
-public class FrameworkTestsAddi {
+public class FrameworkTestsPart2 {
     public static OWWeatherDTO weatherDTO;
 
-    public static List<WeatherItem> weatherItem;
+    public static WeatherItem weatherItem;
 
-*/
 
-    /*
-    @Disabled("Not necessary/working")
-    @DisplayName("Testing empty values")
+    @DisplayName("Testing boundary (below minimum)")
     @Nested
-    class EmptyValues{
+    class outOfBoundMinimum{
         @BeforeAll
         static void setupAll(){
             ObjectMapper mapper = new ObjectMapper();
             try {
-                weatherDTO = mapper.readValue(new File("src/test/java/com/sparta/owframework/filemanager/weatherEmpty.json"), OWWeatherDTO.class);
+                weatherDTO = mapper.readValue(new File("src/test/java/com/sparta/owframework/filemanager/weatherOutOfBounds.json"), OWWeatherDTO.class);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-
-            weatherItem = (WeatherItem) weatherDTO.getWeather();
         }
 
-        @DisplayName("Rain Empty")
+        @DisplayName("Coord below min")
         @Nested
-        class RainEmpty{
-
+        class CoordBelowMin{
             @Test
-            @DisplayName("Check that hasJsonMember1h returns false for empty values ") //note this, rain must be there but null values
-            void checkThatHasJsonMember1hFalseForEmptyRain (){
-                Assertions.assertFalse(weatherDTO.getRain().hasJsonMember1h());
+            @DisplayName("Check that isLatBetweenNeg90AndPos90 false for lower than -90 ") //A
+            void checkThatisLatBetweenNeg90AndPos90FalseLowerThanMinus90 (){
+
+                Assertions.assertFalse(weatherDTO.getCoord().isLatBetweenNeg90AndPos90());
             }
 
             @Test
-            @DisplayName("Check that hasJsonMember3h returns false for empty values ")
-            void checkThatHasJsonMember3hFalseForEmptyRain (){
-                Assertions.assertFalse(weatherDTO.getRain().hasJsonMember3h());
-            }
-        }
-
-        @DisplayName("Snow Empty")
-        @Nested
-        class SnowEmpty{
-
-            @Test
-            @DisplayName("Check that hasJsonMember1h returns false for empty values ") //note this, rain must be there but null values
-            void checkThatHasJsonMember1hFalseForEmptySnow (){
-                Assertions.assertFalse(weatherDTO.getSnow().hasJsonMember1h());
+            @DisplayName("Check that isLongBetweenNeg180AndPos180 is false for lower than -180")
+            void checkThatisLongBetweenNeg180AndPos180FalseLowerThanMinus180 (){
+                Assertions.assertFalse(weatherDTO.getCoord().isLongBetweenNeg180AndPos180());
             }
 
+            @Disabled("Cannot find method for cloud percentage checker")
             @Test
-            @DisplayName("Check that hasJsonMember3h returns false for empty values ")
-            void checkThatHasJsonMember3hFalseForEmptySnow (){
-                Assertions.assertFalse(weatherDTO.getSnow().hasJsonMember3h());
+            @DisplayName("Check that clouds is false for lower than ")
+            void checkThatCloudsIsFalseForLowerThan (){
+                //   Assertions.assertFalse(weatherDTO.getClouds());
             }
         }
 
-        @DisplayName("Sys Empty")
+        @DisplayName("Sys below bound")
         @Nested
-        class SysEmpty{
-
+        class SysBelowBound{
             @Test
-            @DisplayName("Check that hasCountry false for null country")
-            void checkThatHasCountryFalseForNullCountry (){
-                Assertions.assertFalse(weatherDTO.getSys().hasCountry());
-            }
-
-            @Test
-            @DisplayName("Check that hasSunrise false for null Sunrise")
-            void checkThatHasSunriseFalseForNullSunrise (){
-                Assertions.assertFalse(weatherDTO.getSys().hasSunrise());
-            }
-
-            @Test
-            @DisplayName("Check that hasSunset false for null Sunset")
-            void checkThatHasSunsetFalseForNullSunset (){
-                Assertions.assertFalse(weatherDTO.getSys().hasSunset());
-            }
-
-            @Test
-            @DisplayName("Check that hasId false for null Id")
-            void checkThatHasIdFalseForNullId (){
-                Assertions.assertFalse(weatherDTO.getSys().hasId());
-            }
-
-            @Test
-            @DisplayName("Check that hasType false for null Type")
-            void checkThatHasTypeFalseForNullType (){
-                Assertions.assertFalse(weatherDTO.getSys().hasType());
-            }
-
-            @Test
-            @DisplayName("Check that hasMessage false for null message")
-            void checkThatHasMessageFalseForNullMessage (){
-                Assertions.assertFalse(weatherDTO.getSys().hasMessage());
-            }
-
-            @Disabled("Unsure if necessary/logical test")
-            @Test
-            @DisplayName("Check that hasExactlyTwoChars false if country.length == null ")
-            void checkThatHasExactlyTwoCharsFalseIfCountry (){
-            }
-
-
-
-        }
-
-        @DisplayName("WeatherItem Empty")
-        @Nested
-        class WeatherItemEmpty{
-
-            @Test
-            @DisplayName("Check that hasId false if null ID ")
-            void checkThatHasIdFalseIfNullID () {
-                Assertions.assertFalse(weatherItem.hasId());
-            }
-
-            @Test
-            @DisplayName("Check that hasIcon false if null Icon ")
-            void checkThatHasIconFalseIfNullIcon () {
-                Assertions.assertFalse(weatherItem.hasIcon());
-            }
-
-            @Test
-            @DisplayName("Check that hasDescription false if null Description ")
-            void checkThatHasDescriptionFalseIfNullDescription () {
-                Assertions.assertFalse(weatherItem.hasDescription());
-            }
-
-            @Test
-            @DisplayName("Check that hasMain false if null Main ")
-            void checkThatHasMainFalseIfNullMain () {
-                Assertions.assertFalse(weatherItem.hasMain());
+            @DisplayName("Check that hasExactlyTwoChars false if country.length < 2 ")
+            void checkThatHasExactlyTwoCharsFalseIfCountryLessThanTwo (){
+                Assertions.assertFalse(weatherDTO.getSys().hasExactlyTwoChars());
             }
 
         }
@@ -150,10 +64,156 @@ public class FrameworkTestsAddi {
 
     }
 
-     */
+
+    @DisplayName("Testing boundary (above maximum)")
+    @Nested
+    class OutOfBoundsMaximum{
+        @BeforeAll
+        static void setupAll(){
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                weatherDTO = mapper.readValue(new File("src/test/java/com/sparta/owframework/filemanager/weatherOutOfBoundsMax.json"), OWWeatherDTO.class);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @DisplayName("Coord above max")
+        @Nested
+        class CoordAboveMax{
 
 
-/*
+            @Test
+            @DisplayName("Check that isLatBetweenNeg90AndPos90 false for higher than 90 ") //A
+            void checkThatIsLatBetweenNeg90AndPos90FalseHigherThan90 (){
+
+                Assertions.assertFalse(weatherDTO.getCoord().isLatBetweenNeg90AndPos90());
+            }
+
+            @Test
+            @DisplayName("Check that isLongBetweenNeg180AndPos180 is false for higher than 180")
+            void checkThatIsLongBetweenNeg180AndPos180FalseHigherThan180 (){
+                Assertions.assertFalse(weatherDTO.getCoord().isLongBetweenNeg180AndPos180());
+            }
+        }
+
+        @DisplayName("Sys above bound")
+        @Nested
+        class SysAboveBound{
+            @Test
+            @DisplayName("Check that hasExactlyTwoChars false if country.length > 2 ")
+            void checkThatHasExactlyTwoCharsFalseIfCountryMoreThanTwo (){
+                Assertions.assertFalse(weatherDTO.getSys().hasExactlyTwoChars());
+            }
+
+        }
+
+
+    }
+
+    @DisplayName("Testing boundary (on minimum bound)")
+    @Nested
+    class BoundaryValueMin{
+        @BeforeAll
+        static void setupAll(){
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                weatherDTO = mapper.readValue(new File("src/test/java/com/sparta/owframework/filemanager/weatherOnBoundaryMin.json"), OWWeatherDTO.class);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        @DisplayName("Coord on min boundary")
+        @Nested
+        class CoordMinBoundary{
+            @Test
+            @DisplayName("Check that isLatBetweenNeg90AndPos90 true for -90") //A
+            void checkThatIsLatBetweenNeg90AndPos90TrueFor90 (){
+
+                Assertions.assertTrue(weatherDTO.getCoord().isLatBetweenNeg90AndPos90());
+            }
+
+            @Test
+            @DisplayName("Check that isLongBetweenNeg180AndPos180 true for -180")
+            void checkThatIsLongBetweenNeg180AndPos180TrueFor180 (){
+                Assertions.assertTrue(weatherDTO.getCoord().isLongBetweenNeg180AndPos180());
+            }
+        }
+
+    }
+
+    @DisplayName("Testing boundary (on max bound)")
+    @Nested
+    class boundaryValueMax{
+        @BeforeAll
+        static void setupAll(){
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                weatherDTO = mapper.readValue(new File("src/test/java/com/sparta/owframework/filemanager/weatherOnBoundaryMax.json"), OWWeatherDTO.class);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @DisplayName("Coord on max bound")
+        @Nested
+        class CoordMaxBound{
+            @Test
+            @DisplayName("Check that isLatBetweenNeg90AndPos90 true for 90 ") //A
+            void checkThatIsLatBetweenNeg90AndPos90TrueFor90 (){
+                Assertions.assertTrue(weatherDTO.getCoord().isLatBetweenNeg90AndPos90());
+            }
+
+            @Test
+            @DisplayName("Check that isLongBetweenNeg180AndPos180 is true for 180")
+            void checkThatIsLongBetweenNeg180AndPos180TrueFor180 (){
+                Assertions.assertTrue(weatherDTO.getCoord().isLongBetweenNeg180AndPos180());
+            }
+        }
+
+    }
+
+    @DisplayName("Testing invalid values (e.g. min < max) ")
+    @Nested
+    class InvalidValues{
+        @BeforeAll
+        static void setupAll(){
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                weatherDTO = mapper.readValue(new File("src/test/java/com/sparta/owframework/filemanager/Incorrect_weather_data.json"), OWWeatherDTO.class);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @DisplayName("Sys invalid")
+        @Nested
+        class SysInvalid{
+
+            @Test
+            @DisplayName("Check that isSunriseBeforeSunset false for sunset before sunrise ")
+            void checkThatIsSunriseBeforeSunsetFalseForSunsetFirst (){
+                Assertions.assertFalse(weatherDTO.getSys().isSunriseBeforeSunset());
+            }
+
+        }
+
+        @Disabled("Unsure of using weatherItem")
+        @DisplayName("WeatherItem invalid")
+        @Nested
+        class WeatherItemInvalid{
+            @Test
+            @DisplayName("Check that incorrect combination returns false isValidIconDescMainIdCombo")
+            void checkThatIncorrectCombinationReturnsFalseIsValidIconDescMainIDCombo (){
+                Assertions.assertFalse(weatherItem.isValidIconDescMainIdCombo());
+            }
+
+        }
+
+    }
+
     @DisplayName("Testing within boundary")
     @Nested
     class withinBoundary{
@@ -165,8 +225,8 @@ public class FrameworkTestsAddi {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            weatherItem = weatherDTO.getWeather().get(0);
 
+            //  weatherItem = (com.sparta.owframework.OWWeatherDTO.WeatherItem) weatherDTO.getWeather();
         }
 
         @DisplayName("Coord within bounds")
@@ -277,9 +337,11 @@ public class FrameworkTestsAddi {
 
         }
 
+        /*
+        @Disabled("Unsure of using weatherItem")
         @DisplayName("WeatherItem")
         @Nested
-        class WeatherItemTest{
+        class WeatherItem{
 
             @Test
             @DisplayName("Check that hasId true if not null ID ")
@@ -315,16 +377,10 @@ public class FrameworkTestsAddi {
 
         }
 
+         */
     }
 
 
 
 
-
-
-
-
 }
-
- */
-
